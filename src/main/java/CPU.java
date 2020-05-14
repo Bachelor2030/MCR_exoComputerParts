73c5b3332dc8a038c5d08719b5d5ed1645871558
ClarFleu <clarisse.fleu@gmail.com>
2020-05-14 14:34:47 +0200

    Adds the comments for each class and method

--- src/main/java/CPU.java
+++ src/main/java/CPU.java
@@ -1,5 +1,12 @@
+/**
+ * CPU class which represents a CPU that has a discount
+ * Hence, it's extending DiscountedPart
+ */
 public class CPU extends DiscountedPart {
-
+    /**
+     * CPU constructor which creates a CPU with the given original price
+     * @param price (double) the original price of the CPU
+     */
     public CPU(double price) {
         super(price);
     }
--- src/main/java/DiscountedPart.java
+++ src/main/java/DiscountedPart.java
@@ -1,19 +1,39 @@
+/**
+ * DiscountedPart class, which extends Part
+ * A DiscountedPart is a computer Part with a given discount on its price
+ */
 public abstract class DiscountedPart extends Part {
-
+    // Residual price percentage of the part's price
     private static double pricePercent = 1.0;
 
+    /**
+     * DiscountedPart constructor, it creates a discounted part with the given non discounted price
+     * @param price (double) the original price of the part
+     */
     public DiscountedPart(double price) {
         super(price);
     }
 
+    /**
+     * Method used to get the discount percentage of the part
+     * @return pricePercent (double) the percentage of the price a buyer will have to pay
+     */
     public static double getDiscountPercent() {
         return pricePercent;
     }
 
+    /**
+     * Method used to set the discount on the part
+     * @param newDiscount (double) the new discount to add
+     */
     public static void setDiscountPercent(double newDiscount) {
         pricePercent = newDiscount;
     }
 
+    /**
+     * Method used to get the price to pay for the current parrt
+     * @return price (double) the total price to pay
+     */
     @Override
     public double getPrice() {
         return getDiscountPercent() * super.getPrice();
